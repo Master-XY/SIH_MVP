6536533affee9391a95b36e9d6cc6f8bc07d7288
Master-XY <masterxy1857@gmail.com>
2025-09-16 01:00:59 +0530

    redo

--- frontend/pages/4_Species_Map.py
+++ frontend/pages/4_Species_Map.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import requests
 from datetime import datetime, timedelta
-import backend_client
+from frontend import backend_client
 
 
 st.set_page_config(page_title="Species / Occurrences", layout="wide")
